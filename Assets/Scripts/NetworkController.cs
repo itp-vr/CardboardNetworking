5e881a0a7ba343eda311fcdcfd61ec785db3e155
Sean Kelly <sgk291@nyu.edu>
2015-10-20 15:12:56 -0400

    initial commit

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NetworkController : MonoBehaviour
+{
+	string _room = "Cardboard_Networking";
+	
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.1");
+		Debug.Log ("Connected");
+
+
+	}
+	
+	void OnJoinedLobby()
+	{
+		Debug.Log("joined lobby");
+		
+		RoomOptions roomOptions = new RoomOptions() { };
+		PhotonNetwork.JoinOrCreateRoom(_room, roomOptions, TypedLobby.Default);
+	}
+	
+
+	void OnJoinedRoom()
+	{
+
+		//PhotonNetwork.Instantiate("CardboardMain", new Vector3(Random.Range(-5.0F, 5.0F), 0, Random.Range(-5.0F, 5.0F)), Quaternion.identity, 0);
+		PhotonNetwork.Instantiate("NetworkedPlayer", Vector3.zero, Quaternion.identity, 0);
+	}
+}
---
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkedPlayer : Photon.MonoBehaviour
+{
+	public GameObject avatar;
+	
+	public Transform playerGlobal;
+	public Transform playerLocal;
+	
+	void Start ()
+	{
+		Debug.Log("i'm instantiated");
+		
+		if (photonView.isMine)
+		{
+			Debug.Log("player is mine");
+
+			GameObject.Find("CardboardMain").transform.Translate(Random.Range(-4.0F, 4.0F), 0, Random.Range(-4.0F, 4.0F));
+
+			playerGlobal = GameObject.Find("CardboardMain").transform;
+			playerLocal = playerGlobal.Find("Head");
+			
+			this.transform.SetParent(playerLocal);
+			this.transform.localPosition = Vector3.zero;
+			
+			//avatar.SetActive(false);
+		}
+	}
+	
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			stream.SendNext(playerGlobal.position);
+			stream.SendNext(playerGlobal.rotation);
+			stream.SendNext(playerLocal.localPosition);
+			stream.SendNext(playerLocal.localRotation);
+		}
+		else
+		{
+			this.transform.position = (Vector3)stream.ReceiveNext();
+			this.transform.rotation = (Quaternion)stream.ReceiveNext();
+			avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
+			avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
