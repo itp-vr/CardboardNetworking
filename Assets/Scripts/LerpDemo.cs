e33200de2a7721aed74e4b06ab9c92d11ec49036
Chris Kairalla <chris@mailtochris.com>
2015-11-01 15:25:08 -0500

    Added Lerp to Avatar to smooth out motion. Added LerpDemo script to show how lerp works.

---
+++ Assets/Scripts/LerpDemo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LerpDemo : MonoBehaviour {
+	public Vector3 ValueA;
+	public Vector3 ValueB;
+	public float LerpMult;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float lerpTime = LerpMult;
+		Vector3 newValue = Vector3.Lerp(ValueA, ValueB, lerpTime);
+		print ("lerp time:"+lerpTime+", new value:"+newValue);
+	}
+}
--- Assets/Scripts/NetworkedPlayer.cs
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -7,7 +7,20 @@ public class NetworkedPlayer : Photon.MonoBehaviour
 	
 	public Transform playerGlobal;
 	public Transform playerLocal;
+
+	private Vector3 correctAvatarPos = Vector3.zero; //We lerp towards this
+	private Quaternion correctAvatarRot = Quaternion.identity; //We lerp towards this
 	
+	void Update()
+	{
+		if (!photonView.isMine)
+		{
+			//Update remote player (smooth this, this looks good, at the cost of some accuracy)
+			avatar.transform.localPosition = Vector3.Lerp(avatar.transform.localPosition, correctAvatarPos, Time.deltaTime * 5);
+			avatar.transform.localRotation = Quaternion.Lerp(avatar.transform.localRotation, correctAvatarRot, Time.deltaTime * 5);
+		}
+	}
+
 	void Start ()
 	{
 		Debug.Log("i'm instantiated");
@@ -41,8 +54,8 @@ public class NetworkedPlayer : Photon.MonoBehaviour
 		{
 			this.transform.position = (Vector3)stream.ReceiveNext();
 			this.transform.rotation = (Quaternion)stream.ReceiveNext();
-			avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
-			avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+			correctAvatarPos = (Vector3)stream.ReceiveNext();
+			correctAvatarRot = (Quaternion)stream.ReceiveNext();
 		}
 	}
 }
